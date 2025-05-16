d4e6f02f5f772cbe480edb560235f6b8afc5587c
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-05-16 22:59:14 +0100

    filled grid row validation

--- src/solver/grid.ts
+++ src/solver/grid.ts
@@ -7,9 +7,19 @@ export class Grid {
         this.grid = Array.from({length: 9}, () => Array(9).fill(-1))
         cells.forEach(cell => { this.grid[cell.coords.row][cell.coords.column] = cell.value })
     }
+
     public static createCellArray(...cellInputs: [number, number, number][]): Cell[] {
         return cellInputs.map(([val, row, col]) => new Cell(val, row, col))
     }
+    
+    public rowsValid(): boolean {
+        return this.grid.every(row => {
+            for(let i=1; i<=9; i++) if (!row.includes(i)) return false
+            return true
+        })
+    }
 }
 
-    
+
+
+
