4a87e1c94cce78b063f8e44338accfa03d58fb3d
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-05-16 22:31:28 +0100

    range validation

--- src/solver/cell.ts
+++ src/solver/cell.ts
@@ -3,7 +3,9 @@ export class Coords {
     readonly column: number
 
     public constructor(column: number, row: number) {
-        this.row = row
+        if (!(column>=1 && row<=9) && !(row>=1 && row<=9)) 
+            throw new RangeError("Invalid sudoku coordinates")
+        this.row = row 
         this.column = column
     }
 }
