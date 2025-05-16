d8639d8effd8dc8396883c85f8792deb28d83a45
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-05-16 22:27:26 +0100

    coords class

---
+++ src/solver/cell.ts
@@ -0,0 +1,9 @@
+export class Coords {
+    readonly row: number
+    readonly column: number
+
+    public constructor(column: number, row: number) {
+        this.row = row
+        this.column = column
+    }
+}
