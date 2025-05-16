7f5d32754e685053df50e7797510bf16f32ec548
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-05-16 22:41:52 +0100

    cell class

--- src/solver/cell.ts
+++ src/solver/cell.ts
@@ -2,10 +2,20 @@ export class Coords {
     readonly row: number
     readonly column: number
 
-    public constructor(row: number, column: number) {
+    constructor(row: number, column: number) {
         if (row<0 || row>8 || column<0 || column>8) 
             throw new RangeError("Invalid sudoku coordinates")
         this.row = row 
         this.column = column
+    }
+}
+
+export class Cell {
+    readonly value: number
+    readonly coords: Coords
+
+    constructor(value: number, coords: Coords) {
+        this.value = value
+        this.coords = coords
     }
 }
