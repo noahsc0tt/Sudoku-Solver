93eb961bfbf5aa7925ec586c982d16328522dcc0
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-05-17 01:04:23 +0100

    different approach to cell input validation done on the cell[] rather than the constructed grid

--- src/solver/grid.ts
+++ src/solver/grid.ts
@@ -5,34 +5,37 @@ export class Grid {
     
     constructor(cells: Cell[]) {
         this.grid = Array.from({length: 9}, () => Array(9).fill(-1))
-        cells.forEach(cell => { this.grid[cell.coords.row][cell.coords.column] = cell.value })
+        cells.forEach(cell =>
+            this.grid[cell.coords.row][cell.coords.column] = cell.value 
+        )
     }
 
     public static createCellArray(...cellInputs: [number, number, number][]): Cell[] {
         return cellInputs.map(([val, row, col]) => new Cell(val, row, col))
     }
-
-    public gridInputValid(): boolean {
-        return this.rowInputValid() && this.columnInputValid()
-    }
     
-    public rowInputValid(): boolean {
-        return this.grid.every( row => {
-            row.length == (new Set(row)).size
+    public static cellInputsValid(cells: Cell[]): boolean {
+        return cells.every(cell => {
+            let sameValues: Cell[] = cells.filter(c => c.value===cell.value)
+            return sameValues.every( cll => {
+                let sameRow: Cell[] = sameValues.filter(c => c.coords.row===cll.coords.row)
+                let sameColumn: Cell[] = sameValues.filter(c => c.coords.column===cll.coords.column)
+                return sameRow.length===1 && sameColumn.length===1
+            })
         })
     }
     
-    public rowsValid(): boolean {
+    private rowsValid(): boolean {
         return this.grid.every(row => {
-            for(let i=1; i<=9; i++) if (!row.includes(i)) return false
+            for(let n=1; n<=9; n++) if (!row.includes(n)) return false
             return true
         })
     }
     
-    public columnsValid(): boolean {
+    private columnsValid(): boolean {
         
-        for(let n=1; n<=9; n++) {
-            for(let col=0; col<9; col++) {
+        for(let col=0; col<9; col++) {
+            for(let n=1; n<=9; n++) {
                 if (!this.grid.some(row => row[col] === n )) return false
             }
         }
@@ -46,3 +49,9 @@ export class Grid {
     }
 }
 
+function main(): void {
+    let arr: Cell[] = Grid.createCellArray([2,1,3],[2,4,5], [2,2,3])
+    console.log(Grid.cellInputsValid(arr))
+}
+
+main()
