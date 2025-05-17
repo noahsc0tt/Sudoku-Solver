ecf281fb774e49ba9ab1d23fbb8801f0ea1f7b98
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-05-17 18:08:29 +0100

    cellsValid method working for boxes

--- src/solver/grid.ts
+++ src/solver/grid.ts
@@ -14,20 +14,22 @@ export class Grid {
     public static createCellArray(...cellInputs: [number, number, number][]): Cell[] {
         return cellInputs.map(([val, row, col]) => new Cell(val, row, col))
     }
-    
+
     public static cellsValid(cells: Cell[]): boolean {
         let uniqueValueMap: Map<number, number[][]> = new Map()
 
         cells.forEach(cell => {
-            if (!uniqueValueMap.has(cell.value)) uniqueValueMap.set(cell.value, [[],[]])
+            if (!uniqueValueMap.has(cell.value)) uniqueValueMap.set(cell.value, [[],[],[]])
             let coordsArray: number[][] = uniqueValueMap.get(cell.value)!
             coordsArray[0].push(cell.coords.row)
             coordsArray[1].push(cell.coords.column)
+            coordsArray[2].push(Coords.getBox(cell.coords))
         })
         
         for (const entry of uniqueValueMap.entries()) { 
-            if (entry[1][0].length !== (new Set(entry[1][0])).size ||
-            entry[1][1].length !== (new Set(entry[1][1])).size)
+            if (entry[1][0].length !== (new Set(entry[1][0])).size
+            || entry[1][1].length !== (new Set(entry[1][1])).size
+            || entry[1][2].length !== (new Set(entry[1][2]).size))
                 return false
         }
         return true
@@ -41,7 +43,6 @@ export class Grid {
     }
     
     private columnsValid(): boolean {
-        
         for(let col=0; col<9; col++) {
             for(let n=1; n<=9; n++) {
                 if (!this.grid.some(row => row[col] === n )) return false
@@ -50,6 +51,7 @@ export class Grid {
         return true
     }
 
+    
     public isValid(): boolean { return this.columnsValid() && this.rowsValid() }
     
     public printGrid(): void {
@@ -59,7 +61,7 @@ export class Grid {
 
 function main(): void {
     let arr: Cell[] = Grid.createCellArray([2,1,3],[2,4,5], [3,2,3], [3,2,5])
-    console.log(Grid.cellInputsValid(arr))
+    console.log(Grid.cellsValid(arr))
 }
 
 main()
