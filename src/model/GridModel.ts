cd0be5d59a7661ef0850d6bac511b7c711c3af66
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-06 16:42:59 +0100

    removeCell and printCells methods for GridModel

--- src/model/GridModel.ts
+++ src/model/GridModel.ts
@@ -3,9 +3,9 @@ import BruteForcer from "../solver/BruteForcer"
 
 export class GridModelFactory {
     static instance: GridModel | null = null
-    
+
     public static getInstance(): GridModel {
-        if (GridModelFactory.instance === null) 
+        if (GridModelFactory.instance === null)
             GridModelFactory.instance = new GridModel()
         return GridModelFactory.instance
     }
@@ -15,14 +15,27 @@ export class GridModel {
     private valueMap: Map<Coords, number> = new Map()
 
     public setCell(coords: Coords, value: number) {
-        if (value < 1 || value > 9) throw new RangeError("Cell value out of range")
+        if (value < 1 || value > 9)
+            throw new RangeError("Cell value out of range")
         this.valueMap.set(coords, value)
+    }
+
+    public removeCell(coords: Coords): boolean {
+        return this.valueMap.delete(coords)
     }
 
     public solve(): number[][] {
         const cellArray: Cell[] = []
-        this.valueMap.forEach((value, coords) => {cellArray.push(new Cell(value, coords))})
+        this.valueMap.forEach((value, coords) => {
+            cellArray.push(new Cell(value, coords))
+        })
         return BruteForcer.solve(cellArray)
+    }
+
+    public printCells(): void {
+        this.valueMap.forEach((value, coords) =>
+            console.log(`(${coords.row},${coords.column}): ${value}`)
+        )
     }
 
     public clear(): void {
